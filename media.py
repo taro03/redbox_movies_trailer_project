8331669e597cf017710628e64ad439ebfc88ac96
taro03 <taro03@users.noreply.github.com>
2015-07-21 13:56:48 -0700

    Update media.py

--- media.py
+++ media.py
@@ -1,16 +1,19 @@
+#import webbrowser library to open the movies trailer
 import webbrowser
+
+#create class Movie contains constrcutor and method
 class Movie():
+    """create constructor contains information about a movie """
     def __init__(self, movie_title, movie_storyline, poster_image,
                trailer_youtube):
-    """create constructor contains information about a movie """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-
-    def show_trailer(self):
+        
     """create a method that open the movie trailer with
     link from the consstructor"""
+    def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
         
     
