5d0853b22691bac5eaa84a9b90aef8d990d40ae9
taro03 <taro03@users.noreply.github.com>
2015-07-21 22:14:50 -0700

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -8,8 +8,7 @@ import media
 #create instance from class Movie: Forrest Gump
 forrest_gump = media.Movie(
     "Forrest Gump",
-    "Life of Forrest Gump, a slow-witted and naïve, but good-hearted and \
-            athletically prodigious man",
+    "Stars: Tom Hanks, Robin Wright, Gary Sinise",
     "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Forrest_Gump_poster.jpg/220px-Forrest_Gump_poster.jpg",
     "https://www.youtube.com/watch?v=uPIEn0M8su0"
     )
@@ -17,7 +16,7 @@ forrest_gump = media.Movie(
 #create instance from class Movie: Fast and Furious 7
 fast_and_furious7 = media.Movie(
     "Fast And Furious 7",
-    "street clubs that race Japanese cars late at night",
+    "Stars: Vin Diesel, Paul Walker, Dwayne Johnson",
     "https://upload.wikimedia.org/wikipedia/en/thumb/8/8f/Fast_and_Furious_Poster.jpg/220px-Fast_and_Furious_Poster.jpg",
     "https://www.youtube.com/watch?v=hcYKNIEj7co"
     )
@@ -25,8 +24,7 @@ fast_and_furious7 = media.Movie(
 #create instance from class Movie: Minions
 minions = media.Movie(
     "Minions",
-    "Minions are small, yellow pill-shaped creatures who have existed since \
-            the beginning of time",
+    "Stars: Sandra Bullock, Jon Hamm, Michael Keaton ",
     "https://upload.wikimedia.org/wikipedia/en/thumb/3/3d/Minions_poster.jpg/220px-Minions_poster.jpg",
     "https://www.youtube.com/watch?v=dVDk7PXNXB8"
     )
@@ -34,8 +32,7 @@ minions = media.Movie(
 #create instance from class Movie: The Wedding Ringer
 wedding_ringer = media.Movie(
     "Wedding Ringer",
-    "Jimmy Callahan (Kevin Hart) provides best man services for guys who don't \
-            have the friends necessary for a wedding",
+    "Stars: Kevin Hart, Josh Gad, Kaley Cuoco-Sweeting ",
     "https://upload.wikimedia.org/wikipedia/en/thumb/c/c7/TheWeddingRingerPoster.jpg/220px-TheWeddingRingerPoster.jpg",
     "https://www.youtube.com/watch?v=KG5CnCgnplU"
     )
@@ -43,8 +40,7 @@ wedding_ringer = media.Movie(
 #create instance from class Movie: Midnight in Paris
 midnight_in_Paris = media.Movie(
     "Midnight in Paris",
-    "Gil Pender, a successful but creatively unfulfilled Hollywood \
-            screenwriter, and his fiancée, Inez, are in Paris, vacationin",
+    "Stars: Owen Wilson, Rachel McAdams, Kathy Bates",
     "https://upload.wikimedia.org/wikipedia/en/thumb/9/9f/Midnight_in_Paris_Poster.jpg/215px-Midnight_in_Paris_Poster.jpg",
     "https://www.youtube.com/watch?v=FAfR8omt-CY"
     )
@@ -52,8 +48,7 @@ midnight_in_Paris = media.Movie(
 #create instance from class Movie: The Hunger Games
 hunger_games = media.Movie(
     "Hunger games",
-    "The Hunger Games trilogy takes place in an unspecified future time in a \
-            dystopian post-apocalyptic nation of Panem,",
+    "Stars: Jennifer Lawrence, Josh Hutcherson, Liam Hemsworth",
     "https://upload.wikimedia.org/wikipedia/en/thumb/4/42/HungerGamesPoster.jpg/220px-HungerGamesPoster.jpg",
     "https://www.youtube.com/watch?v=KmYNkasYthg"
     )
